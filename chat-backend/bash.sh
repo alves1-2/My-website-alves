d4c691287517bfc6de5c226f9253771836f1cbf6
alves1-2 <nkurikiyimanatheoneste8@gmail.com>
2025-09-27 18:04:35 +0200

    Add files via upload

--- chat-backend/bash.sh
+++ chat-backend/bash.sh
@@ -1,1 +1,8 @@
-
+npm init -y
+npm install express socket.io cors
+mkdir chat-backend
+cd chat-backend
+node server.js
+# To run the server, use the command: node server.js
+# The server will be running on http://localhost:3000
+# Make sure to update your frontend code to connect to this backend server.
